e333764e2255deb992e0d584fbb898e0c60ac0dd
Jin Yu <yjh984@daum.net>
2022-01-30 18:08:13 +0900

    first

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+// import NavBar from "../Components/NavBar";
+import st from "../styles/globals.css";  //can be imported in _app.js.
+import Layout from "../Components/Layout";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}){
+    return(
+        <>
+            {/* <NavBar/>
+            <Component{...pageProps}/> */}
+            {/* <Component/> */}
+            {/* {console.log(Component)} {console.log(pageProps)} */}
+            <Layout>
+                <Component {...pageProps}/>
+            </Layout>
+            {/* <Component {...pageProps}/> */}
+            
+            <style jsx global>{`
+                a {
+                    color : blue;
+                }
+            `}</style>
+        </>
+    )
 }
-
-export default MyApp
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+// import NavBar from "../Components/NavBar";
+import Seo from "../Components/Seo";
+
+export default function About(){
+    return (
+        <div>
+            <Seo title='About'/>
+            <h1> About! </h1>
+        </div>
+    )
+}
