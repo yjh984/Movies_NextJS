57e1b58ad8f8bc16c566c40c6a196be648b5cd36
Jin Yu <yjh984@daum.net>
2022-01-31 23:55:51 +0900

    v0.2 : changing BackEnd rendering

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,62 @@
+// changing a "loading..." to server side rendering
 // import NavBar from "../Components/NavBar";
 import Seo from "../Components/Seo";
 import { useEffect, useState } from "react";
 
-export default function Home(){
-    const [movies,setMovies] = useState();
-    useEffect(()=>{
-        (async()=>{
-            const {results} = await (
-                await fetch('/api/movies')).json();
-            setMovies(results);
-        })();
-    },[]);
+// export default function Home(){
+//     const [movies,setMovies] = useState();
+//     useEffect(()=>{
+//         (async()=>{
+//             const {results} = await (
+//                 await fetch('/api/movies')).json();
+//             setMovies(results);
+//         })();
+//     },[]);
 
+export default function Home({results}){
     return(
         <div className="container">
             <Seo title='Home'/>
-            {!movies && <h4>Loading...</h4>}
-            {movies?.map((movie)=>(
+            {/* {!movies && <h4>Loading...</h4>} */}
+            {/* {movies?.map((movie)=>( */}
+            {results?.map((movie)=>( 
                 <div className="movie" key={movie.id}>
-                    <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
                     <h4>{movie.original_title}</h4>
                 </div>
             ))}
             <style jsx>{`
                 .container {
-                display: grid;
-                grid-template-columns: 1fr 1fr;
-                padding: 20px;
-                gap: 20px;
+                    display: grid;
+                    grid-template-columns: 1fr 1fr;
+                    padding: 20px;
+                    gap: 20px;
+                }
+                .movie{
+                    cursor : pointer;
                 }
                 .movie img {
-                max-width: 100%;
-                border-radius: 12px;
-                transition: transform 0.2s ease-in-out;
-                box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+                    max-width: 100%;
+                    border-radius: 12px;
+                    transition: transform 0.2s ease-in-out;
+                    box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
                 }
                 .movie:hover img {
-                transform: scale(1.05) translateY(-10px);
+                    transform: scale(1.05) translateY(-10px);
                 }
                 .movie h4 {
-                font-size: 18px;
-                text-align: center;
+                    font-size: 18px;
+                    text-align: center;
                 }
             `}</style>
         </div>
-    )
+    );
+}
+
+export async function getServerSideProps(){
+    const {results} = await(
+        await fetch(`http://localhost:3000/api/movies`)).json();
+    return{
+        props:{results},
+    };
 }
